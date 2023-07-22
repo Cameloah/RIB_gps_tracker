d664c1c741392c701ca19350adcad073bac59f8a
Cameloah <koor.jones@gmail.com>
2023-07-22 15:54:19 +0200

    added force update to v2.0.0 for spiffs

--- include/version.h
+++ include/version.h
@@ -11,6 +11,6 @@
 // set the github user and source repo here "user/repo"
 #define GITHUB_REPO     "Cameloah/RIB_gps_tracker"
 // enter the name of the firmware bin file here. A <version> string will be replaced by the FW version
-#define GITHUB_FW_BIN   "firmware.bin"
+#define GITHUB_FW_BIN   "RIB_tracker_<version>.bin"
 // enter the name of the compiled filesystem binary here
 #define GITHUB_FS_BIN   "spiffs.bin"
