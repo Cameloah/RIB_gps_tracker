d78d0e921147915020d0a4d8d94e9eb14ead4f64
Cameloah <koor.jones@gmail.com>
2023-08-27 10:56:54 +0200

    updated version.h

--- include/version.h
+++ include/version.h
@@ -6,7 +6,7 @@
 
 #define FW_VERSION_MAJOR                    2
 #define FW_VERSION_MINOR                    0
-#define FW_VERSION_PATCH                    0
+#define FW_VERSION_PATCH                    1
 
 // set the github user and source repo here "user/repo"
 #define GITHUB_REPO     "Cameloah/RIB_gps_tracker"
