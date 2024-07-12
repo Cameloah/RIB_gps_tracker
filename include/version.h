b1974d8b11adcd23b990afe70250accb0170c841
Cameloah <koor.jones@gmail.com>
2024-07-12 12:22:39 +0200

    chore: Bump FW_VERSION_PATCH to 2

--- include/version.h
+++ include/version.h
@@ -6,7 +6,7 @@
 
 #define FW_VERSION_MAJOR                    2
 #define FW_VERSION_MINOR                    1
-#define FW_VERSION_PATCH                    1
+#define FW_VERSION_PATCH                    2
 
 // set the github user and source repo here "user/repo"
 #define GITHUB_REPO     "Cameloah/RIB_gps_tracker"
